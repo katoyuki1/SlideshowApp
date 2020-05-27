0f50373fe4624622b2d33bfd56856b42efae7db7
yukikato <0me3210g039530z@gmail.com>
2020-05-27 15:23:23 +0900

    issue_3, 6, 7 ・進むボタンで1つ先の画像を表示し、戻るボタンで1つ前の画像を表示 ・最後の画像の表示時に、進むボタンをタップすると、最初の画像が表示される ・最初の画像の表示時に、戻るボタンをタップすると、最後の画像が表示される

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,10 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // 進む
+    @IBOutlet weak var nextButton: UIButton!
+    // 戻る
+    @IBOutlet weak var backButton: UIButton!
+    // 再生・停止
+    @IBOutlet weak var switchButton: UIButton!
+    
+    // 表示している画像の番号
+    var displayImageNo = 0
+    // 画像の名前の配列
+    let imageNameArray = ["cat01", "cat02", "cat03"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //
+        /* 初期画面
+         バンドルした画像ファイルを読み込み
+         Image Viewに画像を設定
+         */
+        let image = UIImage(named: "cat01")
+        imageView.image = image
+    }
+    
+    // 進むボタンをタップ
+    @IBAction func nextButtonTap(_ sender: Any) {
+        if displayImageNo < imageNameArray.count - 1 {
+            // 表示している画像の番号を1増やす
+            displayImageNo += 1
+            // 表示している画像の番号を元に画像を表示する
+            displayImage()
+        } else {
+            displayImageNo = 0
+            displayImage()
+        }
+    }
+    
+    // 戻るボタンをタップ
+    @IBAction func backButtonTap(_ sender: Any) {
+        if displayImageNo >= 1 && displayImageNo <= imageNameArray.count - 1 {
+            // 表示している画像の番号を1減らす
+            displayImageNo -= 1
+            // 表示している画像の番号を元に画像を表示する
+            displayImage()
+        } else {
+            displayImageNo = imageNameArray.count - 1
+            displayImage()
+        }
+    }
+    
+    // 再生・停止ボタンをタップ
+    @IBAction func switchButtonTap(_ sender: Any) {
+        
+    }
+    // 表示している画像の番号を元に画像を表示する
+    func displayImage() {
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[displayImageNo]
+        
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // Image Viewに読み込んだ画像をセット
+        imageView.image = image
     }
 
 
