e9f682392313dcf9c2b13058e8ef6e0488ca0c0a
yukikato <0me3210g039530z@gmail.com>
2020-05-27 18:18:10 +0900

    issue_8,9,10,11 ・再生ボタンをタップすると自動送りが始まり、2秒毎にスライド ・自動送りの間は、進むボタンと戻るボタンはタップ不可 ・再生ボタンをタップすると停止ボタンになり、停止ボタンをタップすると再生ボタンになる ・停止ボタンをタップすると自動送りが止まり、進むボタンと戻るボタンをタップ可能にする

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -24,6 +24,9 @@ class ViewController: UIViewController {
     // 画像の名前の配列
     let imageNameArray = ["cat01", "cat02", "cat03"]
     
+    // 一定の間隔で処理を行うためのタイマー
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -65,8 +68,50 @@ class ViewController: UIViewController {
     
     // 再生・停止ボタンをタップ
     @IBAction func switchButtonTap(_ sender: Any) {
+        // 動作中のタイマーを1つに保つために、 timer が存在しない場合だけ、タイマーを生成して動作させる
+        if self.timer == nil {
+            // タイマーを設定（2秒ごと）
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            
+             // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
+            // ボタンの名前を停止とする
+            switchButton.setTitle("停止", for: .normal)
+            // 背景色・ボタン内テキストサイズを変更
+            self.view.backgroundColor = UIColor.lightGray
+            switchButton.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+            
+        } else if self.timer != nil {
+            // タイマーを停止
+            self.timer.invalidate()
+            // nil にして再び再生(nil の時にタイマー生成)
+            self.timer = nil
+            // 再生・停止ボタンタップ時に進むボタン・戻るボタンのタップ非表示
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+            // ボタンの名前を再生とする
+            switchButton.setTitle("再生", for: .normal)
+            // 背景色・ボタン内テキストサイズを戻す
+            self.view.backgroundColor = UIColor.white
+            switchButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        }
         
     }
+    
+    // #selectorで呼び出される関数
+    @objc func updateTimer(_ timer: Timer) {
+        if displayImageNo < imageNameArray.count - 1 {
+            // 表示している画像の番号を1増やす
+            displayImageNo += 1
+            // 表示している画像の番号を元に画像を表示する
+            displayImage()
+        } else {
+            displayImageNo = 0
+            displayImage()
+        }
+    }
+    
     // 表示している画像の番号を元に画像を表示する
     func displayImage() {
         // 表示している画像の番号から名前を取り出し
@@ -79,6 +124,7 @@ class ViewController: UIViewController {
         imageView.image = image
     }
 
+    
 
 }
 
